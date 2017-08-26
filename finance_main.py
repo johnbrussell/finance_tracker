babe5db579cd5049990c261fc758a3ebb108b1eb
John Russell <john.russell@tufts.edu>
2017-08-26 14:50:19 -0400

    Set up initial structure for code

---
+++ finance_main.py
@@ -0,0 +1,10 @@
+from services.financial_records import FinancialRecords
+
+
+def main():
+    fr = FinancialRecords()
+    fr.interact_with_user()
+
+
+if __name__ == "__main__":
+    main()
---
+++ services/financial_records.py
@@ -0,0 +1,6 @@
+class FinancialRecords:
+    def __init__(self):
+        pass
+
+    def interact_with_user(self):
+        pass
