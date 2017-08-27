e2678c73ef8fbbaeefd591dd565e5b44fe5a3c21
John Russell <john.russell@tufts.edu>
2017-08-27 11:49:21 -0400

    Add code to include the destination of a transaction, the amount of the transaction, and a memo for the transaction

--- services/transactions.py
+++ services/transactions.py
@@ -13,7 +13,6 @@ class Transaction:
     def __init__(self, accounts):
         self.INFORMATION = dict()
         self.ACCOUNTS = [account.lower() for account in accounts]
-        print self.ACCOUNTS
 
     def __create_process(self):
         self.__PROCESS = [
@@ -32,7 +31,7 @@ class Transaction:
         self.__run_process()
 
     def __run_process(self):
-        print("Enter 'back' to return to the prior step; 'cancel' to cancel the new transaction.")
+        print "Enter 'back' to return to the prior step; 'cancel' to cancel the new transaction."
         self.__PROCESS[0](prev=list(), nxt=self.__PROCESS[1:])
 
     def __interpret_result(self, previous_steps, current_step, next_steps, validation, info, key, **kwargs):
@@ -86,14 +85,12 @@ class Transaction:
             if 'does not match format' in str(e):
                 return False
 
-    def __get_from(self, prev=list(), nxt=list(), frm=None):
-        if not frm:
-            frm = raw_input("Enter the source of the money transacted: ").lower()
+    def __get_from(self, prev=list(), nxt=list()):
+        frm = raw_input("Who paid the money transacted?: ").lower()
         frm = self.__detect_account(frm)
         frm = self.__detect_income(frm)
         self.__interpret_result(previous_steps=prev, current_step=self.__get_from, next_steps=nxt,
-                                validation=self.__validate_from, info=frm, key='From',
-                                prev=prev, nxt=nxt, cur=self.__get_from)
+                                validation=self.__validate_from, info=frm, key='From')
 
     def __detect_income(self, frm):
         if frm in self.ACCOUNTS or frm in self.KEYWORDS:
@@ -112,7 +109,8 @@ class Transaction:
 
         distances = [editdistance.eval(frm, account) for account in self.ACCOUNTS]
         for account, distance in zip(self.ACCOUNTS, distances):
-            if distance <= max(abs(len(frm) - len(account)) - 1, min(len(frm), len(account)) / 2):
+            if distance <= max(abs(len(frm) - len(account)) - 1, min(len(frm), len(account)) / 2) or \
+                    account in frm or frm in account:
                 yn = self.__get_yn_response("Did you mean: %s?" % account)
                 if yn in self.KEYWORDS:
                     return self.__back_to_repeat(yn)
@@ -121,17 +119,52 @@ class Transaction:
 
         return frm
 
-    def __validate_from(self, frm, kw_dict):
-        return True
+    @staticmethod
+    def __validate_from(frm):
+        if frm:
+            return True
+        return False
 
     def __get_to(self, prev=list(), nxt=list()):
+        to = raw_input("Who received the money transacted?: ").lower()
+        to = self.__detect_account(to)
+        self.__interpret_result(previous_steps=prev, current_step=self.__get_to, next_steps=nxt,
+                                validation=self.__validate_from, info=to, key='To')
+
+    def __validate_to(self, to):
+        frm = self.INFORMATION['From']
+        if to == frm:
+            return False
+        if to not in self.ACCOUNTS and frm not in self.ACCOUNTS:
+            return False
         return True
 
     def __get_memo(self, prev=list(), nxt=list()):
-        return True
+        memo = raw_input("Enter memo for transaction: ")
+        self.__interpret_result(previous_steps=prev, current_step=self.__get_memo, next_steps=nxt,
+                                validation=self.__validate_memo, info=memo, key='Memo')
+
+    @staticmethod
+    def __validate_memo(memo):
+        if memo:
+            return True
+        return False
 
     def __get_amount(self, prev=list(), nxt=list()):
-        return True
+        amount = raw_input("Enter amount of transaction: ")
+        self.__interpret_result(previous_steps=prev, current_step=self.__get_amount, next_steps=nxt,
+                                validation=self.__validate_amount, info=amount, key='Amount')
+
+    @staticmethod
+    def __validate_amount(amount):
+        try:
+            amount = float(amount)
+        except TypeError as e:
+            if "could not convert" in str(e):
+                return False
+        if amount > 0:
+            return True
+        return False
 
     def __get_categories(self, prev=list(), nxt=list()):
         return True
