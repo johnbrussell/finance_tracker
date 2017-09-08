fe3667f48afdaae68417a3bfb1ee77a1e982e4bd
John Russell <john.russell@tufts.edu>
2017-09-07 20:22:51 -0400

    Add more tests.

--- services/tests/test_transaction.py
+++ services/tests/test_transaction.py
@@ -3,7 +3,7 @@ from services.transactions import Transaction
 
 
 class TestTransactions(unittest.TestCase):
-    ACCOUNTS = ['TestAccount']
+    ACCOUNTS = ['TestAccount', 'AnotherTestAccount']
     TRANSACTION = Transaction(accounts=ACCOUNTS)
 
     def test_account_check(self):
@@ -22,6 +22,25 @@ class TestTransactions(unittest.TestCase):
     def test_validate_from(self):
         self.assertFalse(self.TRANSACTION._validate_from(''))
         self.assertTrue(self.TRANSACTION._validate_from('any_input'))
+
+    def test_validate_to(self):
+        self.TRANSACTION.INFORMATION = {"From": 'TestAccount'}
+        self.assertFalse(self.TRANSACTION._validate_to('TestAccount'))
+        self.assertTrue(self.TRANSACTION._validate_to('NotAnAccount'))
+        self.assertTrue(self.TRANSACTION._validate_to('AnotherTestAccount'))
+
+        self.TRANSACTION.INFORMATION = {"From": 'NotAnAccount'}
+        self.assertFalse(self.TRANSACTION._validate_to('AlsoNotAnAccount'))
+        self.assertTrue(self.TRANSACTION._validate_to('TestAccount'))
+
+    def test_validate_memo(self):
+        self.assertFalse(self.TRANSACTION._validate_memo(''))
+        self.assertTrue(self.TRANSACTION._validate_memo('any_input'))
+
+    def test_validate_amount(self):
+        self.assertFalse(self.TRANSACTION._validate_amount(-1))
+        self.assertFalse(self.TRANSACTION._validate_amount(0))
+        self.assertFalse(self.TRANSACTION._validate_amount("One Hundred"))
 
 
 if __name__ == "__main__":
